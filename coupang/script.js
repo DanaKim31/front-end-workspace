e24846649e8c57961eec549f1234de6ac3102298
Danakim20 <152463101+Danakim20@users.noreply.github.com>
2023-12-20 18:16:39 +0900

    클론코딩_쿠팡(진행 중)

--- coupang/script.js
+++ coupang/script.js
@@ -14,28 +14,42 @@ navIcons[1].addEventListener("click", function () {
   });
 });
 
-// category
-const categoryBtn = document.querySelector(".category-btn");
-const category = document.querySelector(".category");
+// main
+const mainImg = document.querySelectorAll("main > img");
 
-categoryBtn.addEventListener("mouseenter", function () {
-  category.style.display = "flex";
-});
+// 첫 번째 이미지에 main-img-active 클래스가 추가되어 있어요!
+setInterval(() => {
+  for (let i = 0; i < mainImg.length; i++) {
+    const className = "main-img-active";
 
-category.addEventListener("mouseleave", function () {
-  category.style.display = "none";
-});
+    // 첫 번째 이미지에서는 클래서 제거, 두 번째 이미지에 클래스 추가
+    // 두 번째 이미지에서는 클래스 제거, 세 번째 이미지에 클래스 추가
+    // 마지막 이미지에서는 클래스 제거, 첫 번째 이미지에 클래스 추가
 
-const categoryItems = document.querySelectorAll(".category-item");
-const categorySub = document.querySelector(".category-sub");
-const categorySubItems = document.querySelectorAll(".category-sub-item");
+    if (mainImg[i].classList.contains(className)) {
+      mainImg[i].classList.remove(className);
 
-categoryItems.forEach((item, index) => {
-  item.addEventListener("mouseenter", function () {
-    categorySub.style.display = "block";
-    categorySubItems[index].style.display = "block";
-  });
-  item.addEventListener("mouseleave", function () {
-    categorySubItems[index].style.display = "none";
-  });
+      // 마지막번째만 예외 처리
+      if (i === mainImg.length - 1) {
+        mainImg[0].classList.add(className);
+        break;
+      }
+
+      mainImg[i + 1].classList.add(className);
+      break;
+    }
+  }
+  // 2초(2000) 간격으로 이미지 변환
+}, 2000);
+
+// 사용자가 스크롤을 내리는 순간 어느 시점에 나타나고
+// 다시 올리면 사라지는..!
+const categoryMenu = document.querySelector(".category-menu");
+window.addEventListener("scroll", function () {
+  console.log(window.scrollY);
+  if (this.window.scrollY >= 1300) {
+    categoryMenu.style.display = "flex";
+  } else {
+    categoryMenu.style.display = "none";
+  }
 });
