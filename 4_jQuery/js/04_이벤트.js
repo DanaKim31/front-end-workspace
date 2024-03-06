7da11de1217179cc77b86078b1ac83735d9e9ce9
DanaKim31 <152463101+DanaKim31@users.noreply.github.com>
2024-03-06 18:22:42 +0900

    jQuery  04_이벤트, 05_효과

--- 4_jQuery/js/04_이벤트.js
+++ 4_jQuery/js/04_이벤트.js
@@ -80,8 +80,45 @@ $("#textarea1").on({
 });
 
 // 동적으로 글자 수 세기
-
 $("#textarea2").keyup((e) => {
-  $("#counter").text(count(e.keyup));
-  console.log(`keyup - e.key : ${e.key}, e.keyCode : ${e.keyCode}`);
+  let target = $(e.target);
+  let currentLength = target.val().length; // textarea2의 value 값에 접근 후 값의 길이
+
+  let maxLength = parseInt($("#maxLength").text()); // maxLength의 타입 : String 이기 때문에 parseInt를 써서 Int로 형 변환
+  // console.log(typeof maxLength);
+
+  if (currentLength > maxLength) {
+    target.val(target.val().substr(0, maxLength));
+  } else {
+    $("#counter").text(currentLength); // counter 텍스트 변환
+  }
+});
+
+// 동적으로 아이디 조건 확인
+$("#userId").keyup((e) => {
+  let id = $(e.target).val(); // 인풋 입력값 접근 - 제이쿼리 방식
+  id = e.target.value; // 인풋 입력값 접근 - 자바스크립트 방식
+
+  console.log(id);
+  const regExp = /^[a-z][a-z0-9]{3,11}$/; // [a-z]로 첫 글자를 빼뒀기 때문에 {3,11}
+  console.log(regExp.test(id));
+
+  if (regExp.test(id)) {
+    $("#idCheck").text("사용 가능한 아이디입니다.").css("color", "green");
+  } else if (id === "") {
+    $("#idCheck").text("");
+  } else {
+    $("#idCheck").text("사용 불가능한 아이디입니다.").css("color", "red");
+  }
+});
+
+// 3. trigger() 메서드
+$("#area3").click(() => {
+  let counter = $("#counter2");
+  let currentCount = parseInt(counter.text());
+  counter.text(++currentCount);
+});
+
+$("#btn").click(() => {
+  $("#area3").trigger("click"); // area3에 걸려있는 click 이벤트 가져오기
 });
